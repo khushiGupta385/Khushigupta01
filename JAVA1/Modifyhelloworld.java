058e617c43338c011b38fc1f92ea46af1d716f87
khushi gupta <khushisatna385@gmail.com>
2025-11-22 11:35:30 +0530

    Add files via upload

---
+++ JAVA1/Modifyhelloworld.java
@@ -0,0 +1,7 @@
+public class Modifyhelloworld {
+    public static void main(String[]args) {
+        System.out.println("hello world");
+        System.out.println("java language");
+        System.out.println("java is a programming language");
+    }   
+}
