7c87edff18db22c5a225e1e5e16411d066398ad4
khushi gupta <khushisatna385@gmail.com>
2025-11-22 11:37:15 +0530

    Add files via upload

---
+++ JAVA1/Collegename.java
@@ -0,0 +1,8 @@
+public class Collegename {
+    public static void main (String[]args){
+        String CollegeName = "SAIT";
+        for(int i=0;i<CollegeName.length();i++){
+            System.out.println(CollegeName.charAt(i));
+        }
+    }
+}
---
+++ JAVA1/Todaydate.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+public class Todaydate {
+    public static void main(String[]args) {
+        LocalDate today = LocalDate.now();
+        System.out.println("today date is: "+today);
+    }
+}
